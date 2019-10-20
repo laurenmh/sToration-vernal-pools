e3685283265efe99183a4ecd3633e24d8f28e250
Lauren Shoemaker <lash1937@gmail.com>
2019-10-20 08:34:57 -0600

    added sourcing data code

---
+++ example_analysis.R
@@ -0,0 +1,6 @@
+# Example script for sourcing data the begin analysis
+
+# source the data
+source("data_compiling/compile_composition.R")
+source("data_compiling/compile_constructed_depth.R")
+source("data_compiling/compile_reference_depth.R")
