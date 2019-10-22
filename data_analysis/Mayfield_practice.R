7dd5e413efc4cc5043369f216d444cdf2374dd10
Margie.Mayfield <you@example.com>
2019-10-22 23:04:22 +1000

    Margie's practice

---
+++ data_analysis/Mayfield_practice.R
@@ -0,0 +1,9 @@
+# Example script for sourcing data the begin analysis
+
+# source the data
+source("data_compiling/compile_composition.R")
+source("data_compiling/compile_constructed_depth.R")
+source("data_compiling/compile_reference_depth.R")
+
+# graph constructed pond duration over time
+ggplot(constructed_duration, aes(x = Year, y = Duration.weeks)) + geom_point()
