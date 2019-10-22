6eec5730d5f15504aa0a152982a33a7b7634d483
Lauren Hallett <hallett@uoregon.edu>
2019-10-22 10:12:19 +0200

    demo graph of pool depth

--- data_analysis/example_analysis.R
+++ data_analysis/example_analysis.R
@@ -4,3 +4,6 @@
 source("data_compiling/compile_composition.R")
 source("data_compiling/compile_constructed_depth.R")
 source("data_compiling/compile_reference_depth.R")
+
+# graph constructed pond duration over time
+ggplot(constructed_duration, aes(x = Year, y = Duration.weeks)) + geom_point()
