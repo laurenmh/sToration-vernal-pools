264b584ed618097123b1027e08decf459ce2b4f8
Lina Aoyama <laoyama@uoregon.edu>
2019-10-22 06:02:03 -0700

    practice facet grid

--- data_analysis/lina_practice.R
+++ data_analysis/lina_practice.R
@@ -3,4 +3,4 @@ source("data_compiling/compile_constructed_depth.R")
 source("data_compiling/compile_reference_depth.R")
 
 # graph constructed pond duration over time
-ggplot(ref_depth, aes(x = Date, y = Depth)) + geom_point()
+ggplot(ref_depth, aes(x = Date, y = Depth)) + geom_point() + facet_grid (.~ Year)
