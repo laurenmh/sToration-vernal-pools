35734c5bb7256e465b83bea50b09bc758a922085
Loralee Larios <loralee@ucr.edu>
2019-10-22 14:50:27 +0200

    new edit

--- data_analysis/Loralee_practice.R
+++ data_analysis/Loralee_practice.R
@@ -5,3 +5,5 @@ source("data_compiling/compile_reference_depth.R")
 
 # graph constructed pond duration over time
 ggplot(constructed_duration, aes(x = Year, y = Duration.weeks)) + geom_point()+facet_wrap(~Size)
+
+head(constructed_duration)
