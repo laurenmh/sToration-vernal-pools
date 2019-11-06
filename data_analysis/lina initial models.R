e2c75a2b0fde2349961eb76da0179dbc7115de6d
Lina Aoyama <laoyama@uoregon.edu>
2019-11-06 07:45:18 -0800

    syntax edit: periods swapped with underscore in object names

--- data_analysis/lina initial models.R
+++ data_analysis/lina initial models.R
@@ -7,37 +7,50 @@ library(rstan)
 
 # source the data
 source("data_compiling/compile_composition.R")
-source("data_compiling/compile_constructed_depth.R")
-source("data_compiling/compile_reference_depth.R")
+LACOseeds <- read.csv(paste(datpath, "/Seed count data/Vernal Pool LACO seed count data.csv", sep="")) 
+
+# get distribution the seed production of LACO to inform prior for lambda
+restoredLACO <- LACOseeds %>%
+  filter(Pool.Type == "Restored")
+hist(restoredLACO$X..Potentially.Viable)
+mean(restoredLACO$X..Potentially.Viable)
+sd(restoredLACO$X..Potentially.Viable)
 
 # Stan model
 BH_model <- stan_model(model_code="
 data{
     int n; // number of observations
-    int n.pools; // number of pools
-    vector[n.pools] pool.id; // pool id
-    int n.years; // number of years
-    vector[n.years] year; // year
-    int [n.pools, year] obs.LACO; // LACOdens
-    int [n.pools, year] obs.EG; // exotic grass density
-    int [n.pools, year] obs.ERVA; // ERVAdens
-    int [n.pools, year] obs.NF; // non-native forb density
-    vector[year] g.LACO; // growth rate of LACO
-    real s.LACO; // survival rate of LACO
+    int n_pools; // number of pools
+    vector[n_pools] pool_id; // pool id
+    int n_years; // number of years
+    vector[n_years] year; // year
+    int obs_LACO [n_pools, n_years]; // LACOdens
+    int obs_EG [n_pools, n_years]; // exotic grass density
+    int obs_ERVA [n_pools, n_years]; // ERVAdens
+    int obs_NF [n_pools, n_years]; // non-native forb density
+    vector[n_years] g_LACO; // growth rate of LACO
+    real s_LACO; // survival rate of LACO
 }
 parameters{
-    matrix<lower = 0>[n.pools, year] lambda; // max growth rate of LACO in absence of competition
-    matrix<lower = 0>[n.pools, year] alpha.LACO; // competition term for LACO-LACO
-    matrix<lower = 0>[n.pools, year] alpha.EG; // competition term for LACO-exotic grass
-    matrix<lower = 0>[n.pools, year] alpha.ERVA;// competition term for LACO-ERVA
-    matrix<lower = 0>[n.pools, year] alpha.NF; // competition term for LACO-non-native forb
+    matrix<lower = 0>[n_pools, n_years] lambda; // max growth rate of LACO in absence of competition
+    matrix<lower = 0>[n_pools, n_years] alpha_LACO; // competition term for LACO-LACO
+    matrix<lower = 0>[n_pools, n_years] alpha_EG; // competition term for LACO-exotic grass
+    matrix<lower = 0>[n_pools, n_years] alpha_ERVA;// competition term for LACO-ERVA
+    matrix<lower = 0>[n_pools, n_years] alpha_NF; // competition term for LACO-non-native forb
 }
 model{
-    for(j in 1:n.pools){
-        obs.LACO[j,] ~ dpois(100, g.LACO[1]); // this g.LACO[1] is just first year's growth rate
+    for(j in 1:n_pools){
+        obs_LACO[j,] ~ poisson(100, g_LACO); // this g_LACO[1] is just first year's growth rate
     }
     for(i in 1:n.years){
-
+        obs_LACO[,i] ~ (g_LACO[i-1] * obs_LACO[,i-1] * lambda[,i-1])/(1 + obs_LACO[,i-1] * alpha_LACO[,i-1] + 
+        obs_EG[,i-1] * alpha_EG[,i-1] + obs_ERVA[,i-1] * alpha_ERVA[,i-1] + obs_NF[,i-1] * alpha_NF[,i-1]) + 
+        s_LACO * (1 - g_LACO[i-1]) * obs_LACO[,i-1]);
     }
+    lambda ~ normal(122.2561,83.95284);
+    alpha_LACO ~ normal(0,1);
+    alpha_EG ~ normal(0,1);
+    alpha_ERVA ~ normal(0,1);
+    alpha_NF ~ normal(0,1);
 }")
 
