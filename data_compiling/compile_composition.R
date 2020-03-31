db9564ca3046c13814900480b74ba498a2ab4c97
Lina Aoyama <laoyama@uoregon.edu>
2020-03-31 11:33:14 -0700

    read in data from 2000 and add to master data

--- data_compiling/compile_composition.R
+++ data_compiling/compile_composition.R
@@ -9,6 +9,12 @@ const_com <- read_csv(paste(datpath, "Aboveground annual vegetation data/dat_csv
   mutate(Treatment.1999 = fct_recode(Treatment.1999, "Group A" = "Group 1", "Group B" = "Group 2")) %>%
   mutate(Treatment.2000 = fct_recode(Treatment.2000, "Group A" = "Group 1", "Group B" = "Group 2"))
 
+const_com_2000 <- read_csv(paste(datpath, "Aboveground annual vegetation data/2000 veg data CONSTRUCTED pools.csv", sep = "")) %>%
+  rename(Treatment.1999 = `Treatment 1999`,
+         Treatment.2000 = `Treatment 2000`) %>%
+  mutate(Year = "2000")
+
+const_com <-  rbind(const_com, const_com_2000)
 
 ## Read in data from the reference vernal pools
 ref_com <- read_csv(paste(datpath, "Aboveground annual vegetation data/dat_csv_reference_pools.csv", sep="")) 
