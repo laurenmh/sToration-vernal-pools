00502119989c90ffd6e5d2047431a8d361c83db5
Lina Aoyama <laoyama@uoregon.edu>
2020-05-12 16:58:35 -0700

    LACO and inundation analysis

--- data_analysis/analysis inundation.R
+++ data_analysis/analysis inundation.R
@@ -1,14 +1,27 @@
-# Run complex_belowground_v2 with multiple year seeding addition
+# Run "prep data before modeling.R" then "complex_belowground_v3.R" first.
+
 # Question:
 ## How does the environment, namely inundation, change LACO growth rate and competition interactions?
+
 # Outline:
-## I. Inundation vs. lambda
-## II. Inundation vs. alpha
+## I. Env vs. LACO counts
+## II. Inundation vs. lambda
+## III. Inundation vs. alpha
+
+# Set up:
 library(ggplot2)
 
 se<-function(x){
   sd(x)/sqrt(length(x))
 } # this is a function for calculating standard error
+
+PPT <- read_csv(paste(datpath, "Monthly precip averages/Fairfield_precip.csv", sep="")) 
+source("data_compiling/compile_constructed_depth.R")
+source("data_compiling/compile_reference_depth.R") #I haven't use this yet
+
+##########################
+# I. Env vs. LACO counts #
+##########################
 
 #Does the pond size matter?
 #-> what do the timeseries of LACO look like by pond size?
@@ -39,7 +52,57 @@ long_relative_GR <- relative_GR %>% gather(`2001`, `2002`, `2003`, `2003`, `2004
 ggplot(long_relative_GR, aes(x = Year, y = log(GR), col = Size)) +
   geom_jitter() #no difference in LACO growth rates among pond sizes
 
-### I. INUNDATION VS. LAMBDA ###
+#Which environmental variable is important for LACO?
+#-> what does the correlation matrix of LACO stem counts, early ppt, max depth, inundation duration look like?
+inundation_summary <- const_depth %>%
+  filter(Location == "SeedPlot", Treatment.1999 != "Control") %>%
+  group_by(Year, Pool, Distance, Size) %>%
+  summarize(max_depth = as.numeric(max(Depth)),
+            duration_wk = as.numeric(max(Duration.weeks))) #inundation data available for 2000, 2002, 2009, 2010, 2011, 2012
+
+env_summary <- left_join(inundation_summary, PPT%>% select(Year, Jan_March_cm), by = "Year") 
+env_summary$Year <- as.character(env_summary$Year)
+LACO_env <- left_join(env_summary, const_com%>%select(Year, Pool, LACOdens), by = c("Year", "Pool")) %>%
+  drop_na() #use this combined data
+
+pairs(~log(LACOdens)+max_depth+duration_wk+Jan_March_cm, data = LACO_env) #this makes a pairwise comparison  
+LACO_env_cor <- cor(LACO_env[,5:8], method = c("pearson")) #this makes a correlation matrix
+library(corrplot)
+corrplot(LACO_env_cor, type = "upper", order = "hclust") #this makes a correlogram
+library(Hmisc)
+rcorr(as.matrix(LACO_env[,5:8])) #this makes a correlation matrix with significance levels
+    #LACO count looks positively correlated with max depth (increases up to 10cm)
+    #max depth and duration are strongly positively correlated
+    #early PPT is lightly correlated with max depth
+    #early PPT is lightly correlated with LACO count
+
+ggplot(LACO_env, aes(x = max_depth, y = log(LACOdens))) +
+  geom_point() #LACO count pos correlates with max depth 
+
+ggplot(LACO_env, aes(x = Jan_March_cm, y = max_depth)) +
+  geom_jitter() #max depth pos correlates with early PPT
+
+#-> What does the overall effct of ppt on LACO?
+PPT$Year <- as.character(PPT$Year)
+LACO_ppt <- left_join(const_com %>% select(Year, Size, Pool, LACOdens), PPT %>% select(Year, Jan_March_cm, Jan, Feb, March), by = "Year") %>%
+  drop_na()
+summary(lm(LACOdens ~ Jan_March_cm, LACO_ppt)) #ppt has a significant effect on LACO counts
+ggplot(LACO_ppt, aes(x = Jan_March_cm, y = log(LACOdens), col = Size))+
+  geom_point()
+
+#-> what are the effects of pool size or distance from runway on pool depth?
+summary(lm(max_depth ~ Size, inundation_summary)) #no effect of size on max_depth
+summary(lm(max_depth ~ Distance, inundation_summary)) #no effect of distance on max_depth
+
+ggplot(inundation_summary, aes(x = as.factor(Year), y = max_depth, color = Size)) +
+  geom_jitter()
+ggplot(inundation_summary, aes(x = as.factor(Year), y = max_depth, color = Distance)) +
+  geom_jitter()
+
+
+#############################
+# II. INUNDATION VS. LAMBDA #
+#############################
 
 #1.Extract lambda values from the model
 
@@ -56,31 +119,7 @@ lambda_summary <- lambda_gather %>%
   summarize(mean_lambda = mean(lambda), se_lambda = se(lambda)) #this is used for plotting
 
 #2.Regression of max water depth and precipitation to predict inundation of all years
-
-PPT <- read_csv(paste(datpath, "Monthly precip averages/Fairfield_precip.csv", sep="")) 
-source("data_compiling/compile_constructed_depth.R")
-
-max_depth <- const_depth %>%
-  filter(Location == "SeedPlot", Treatment.1999 != "Control") %>%
-  group_by(Year, Pool, Distance, Size) %>%
-  summarize(max_depth = as.numeric(max(Depth)))
-
-summary(lm(max_depth ~ Size, max_depth)) #no effect of size on max_depth
-summary(lm(max_depth ~ Distance, max_depth)) #no effect of distance on max_depth
-
-ggplot(max_depth, aes(x = as.factor(Year), y = max_depth, color = Size)) +
-  geom_point()
-ggplot(max_depth, aes(x = as.factor(Year), y = max_depth, color = Distance)) +
-  geom_point()
-
-PPT_subset <- PPT %>%
-  select(Year, Jan_March_cm) %>%
-  filter(Year %in% c(2000, 2002, 2009, 2010, 2011, 2012)) #subset the precip data by specific year
-
-PPT_depth <- left_join(max_depth, PPT_subset, by = "Year") %>%
-  filter(max_depth != 'NA') #combine ppt and water depth data
-
-summary(lm(max_depth ~ Jan_March_cm, PPT_depth)) #max water depth is significantly correlated with Jan - March precipitation
+summary(lm(max_depth ~ Jan_March_cm, env_summary)) #max water depth is significantly correlated with Jan - March precipitation
 # Equation: max_depth = 5.09207 + 0.03201 * PPT_Jan_March_cm #R-squared is 0.007
 
 ggplot() +
@@ -114,4 +153,6 @@ ggplot(lambda_depth, aes(x = pred_max_depth, y = lambda)) +
 
 summary(lm(pred_max_depth ~ lambda, lambda_depth)) 
 
-### II. INUNDATION VS. ALPHA ###
+#############################
+# III. INUNDATION VS. ALPHA #
+#############################
