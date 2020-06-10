1bf78ef3987aee2e02aed137ad66f59dcf919031
Lina Aoyama <laoyama@uoregon.edu>
2020-06-10 15:55:45 -0700

    revise the timeseries figures - reference and constructed to fit in one graph

--- data_analysis/timeseries visuals.R
+++ data_analysis/timeseries visuals.R
@@ -8,40 +8,63 @@ library(ggplot2)
 library(ggpubr)
 
 # Convert LACO frequency to density in REFERENCE pools
-ref_LACOden <- ref_com_LACO %>%
+ref_LACO <- ref_com_LACO %>% #7pools
   group_by(Pool) %>%
-  gather(key = "Year", value = "LACOcover" , - Pool) %>%
-  mutate(refLACOdens = round(exp(-0.42)+LACOcover^1.36))
-ref_LACOden$Year <- as.numeric(ref_LACOden$Year)
+  gather(key = "Year", value = "LACO" , - Pool) %>%
+  mutate(LACOdens = round(exp(-0.42)+LACO^1.36)) %>%
+  mutate(type = "reference")
+ref_LACO$Year <- as.numeric(ref_LACO$Year)
 
-# Truncate CONSTRUCTED pool data to 2000-2015
-const_LACOden <- const_com_noNA %>%
+# Truncate CONSTRUCTED pool LACO data to 2000-2015
+const_LACOden <- const_com_noNA %>% #72 pools
   select(- c(Size, `2016`, `2017`)) %>%
   group_by(Pool) %>%
-  gather(key = "Year", value = "constLACOdens", -Pool) 
-const_LACOden$Pool <- as.character(const_LACOden$Pool)
-const_LACOden$Year <- as.numeric(const_LACOden$Year)
+  gather(key = "Year", value = "LACOdens", -Pool) %>%
+  mutate(type = "constructed")
+const_LACO <- left_join(const_LACOden, (const_com %>% select (Year, Pool, LACO)))
+const_LACO$Pool <- as.character(const_LACO$Pool)
+const_LACO$Year <- as.numeric(const_LACO$Year)
+const_LACO$LACO <- as.numeric(const_LACO$LACO)
 
 # Join tables
-join_LACOden <- full_join(ref_LACOden, const_LACOden, all = TRUE)
+join_LACO <- full_join(ref_LACO, const_LACO, all = TRUE)
 
-# Visualize separately
-ref <- ggplot(ref_LACOden, aes(x = Year, y = refLACOdens)) +
+# Visualize LACOdens separately
+ref <- ggplot(ref_LACO, aes(x = Year, y = LACOdens)) +
   geom_jitter(col = "blue") + 
   geom_smooth(method = "loess") +
   ylab("Reference pools LACO density") +
   theme_bw()
-const <- ggplot(const_LACOden, aes(x = Year, y = constLACOdens)) +
+const <- ggplot(const_LACO, aes(x = Year, y = LACOdens)) +
   geom_jitter(col = "pink") + 
   geom_smooth(method = "loess", col = "red") +
   ylab("Constructed pools LACO density") +
   theme_bw()
 ggarrange(ref, const, ncol = 1, nrow = 2)
 
-# Visualize together
-ggplot(join_LACOden, aes(x = Year)) +
-  geom_jitter(aes(y = log(refLACOdens)), col = "lightblue") +
-  geom_jitter(aes(y = log(constLACOdens)), col = "pink") + 
-  geom_smooth(aes(y = log(refLACOdens)), method = "loess", na.rm = TRUE) +
-  geom_smooth(aes(y = log(constLACOdens)), method = "loess", na.rm = TRUE, col = "red") +
+# Visualize LACOdens together
+ggplot(join_LACO, aes(x = Year, y = LACOdens, col = type)) +
+  geom_jitter() +
+  geom_smooth() +
+  theme_bw() +
+  ylab("LACO density")
+
+# Visualize LACO cover seperately
+refcov <- ggplot(ref_LACO, aes(x = Year, y = LACO)) +
+  geom_jitter(col = "blue") +
+  geom_smooth(method = "loess") +
+  ylab("Reference pools LACO frequency") +
   theme_bw()
+constcov <- ggplot(const_LACO, aes(x = Year, y = LACO)) +
+  geom_jitter(col = "pink") +
+  geom_smooth(method = "loess", col = "red") +
+  ylab("Constructed pool LACO frequency") +
+  theme_bw()
+ggarrange(refcov, constcov, ncol = 1, nrow  =2)
+
+# Visualize LACO cover together
+ggplot(join_LACO, aes(x = Year, y = LACO, col = type)) + 
+  geom_jitter() +
+  geom_smooth() +
+  theme_bw() +
+  ylab("LACO frequency (%)")
