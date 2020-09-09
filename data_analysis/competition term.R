cabd8c10df0e2d2afbb4e89a0ac749e67dc37908
Lina Aoyama <laoyama@uoregon.edu>
2020-09-09 14:58:44 -0700

    competition term exotic grass only or LACO only

--- data_analysis/competition term.R
+++ data_analysis/competition term.R
@@ -32,18 +32,22 @@ NF_mean <- const_dummy_join %>%
 #Reference pools
 refLACO_mean <- ref_com_join %>%
   group_by(Year) %>%
-  summarise(refLACO_mean = mean(LACO))
+  summarise(refLACO_mean = mean(LACO)) %>%
+  as.matrix()
 refEG_mean <- ref_com_join %>%
   group_by(Year) %>%
-  summarise(refEG_mean = mean(sum_EG))
+  summarise(refEG_mean = mean(sum_EG)) %>%
+  as.matrix()
 refERVA_mean <- ref_com_join %>%
   group_by(Year) %>%
-  summarise(refERVA_mean = mean(ERVA))
+  summarise(refERVA_mean = mean(ERVA)) %>%
+  as.matrix()
 refNF_mean <- ref_com_join %>%
   group_by(Year) %>%
-  summarise(refNG_mean = mean(sum_NF))
+  summarise(refNG_mean = mean(sum_NF)) %>%
+  as.matrix()
 
-# Calculate the competition term
+# Calculate the competition term - inter and intra-specific competition
 Cx <- c()
 comp.fxn <- function(aii, a1, a2, a3, LACO, EG, ERVA, NF){
   for(i in 1:17){
@@ -52,6 +56,12 @@ comp.fxn <- function(aii, a1, a2, a3, LACO, EG, ERVA, NF){
 return(Cx)
 }
 
+comp.ref.fxn <- function(aii, a1, a2, a3, LACO, EG, ERVA, NF){
+  for(i in 1:13){
+    Cx[i] = 1-(1/(1+(aii[i]*LACO[i])+(a1[i]*EG[i])+(a2[i]*ERVA[i])+(a3[i]*NF[i])))
+  }  
+  return(Cx)
+}
 comp <- comp.fxn(aii = alpha_LACO_mean[,5],
                  a1 =  alpha_EG_mean[,5],
                  a2 = alpha_ERVA_mean[,5],
@@ -62,14 +72,14 @@ comp <- comp.fxn(aii = alpha_LACO_mean[,5],
                  NF = as.numeric(NF_mean[,2])) %>%
         as.data.frame() # for constructed pools
 
-comp_ref <- comp.fxn(aii = refalpha_LACO_mean[,5],
+comp_ref <- comp.ref.fxn(aii = refalpha_LACO_mean[,5],
                      a1 =  refalpha_EG_mean[,5],
                      a2 = refalpha_ERVA_mean[,5],
                      a3 = refalpha_NF_mean[,5],
-                     LACO = as.numeric(LACO_mean[,2]),
-                     EG = as.numeric(EG_mean[,2]),
-                     ERVA = as.numeric(ERVA_mean[,2]),
-                     NF = as.numeric(NF_mean[,2])) %>%
+                     LACO = as.numeric(refLACO_mean[,2]),
+                     EG = as.numeric(refEG_mean[,2]),
+                     ERVA = as.numeric(refERVA_mean[,2]),
+                     NF = as.numeric(refNF_mean[,2])) %>%
             as.data.frame()# for reference pools
 
 # Make a lambda table  2003-2015
@@ -97,11 +107,94 @@ join_comp_lambda <- left_join(lambda_const_ref, comp_const_ref, by = c("Year", "
 
 # Plot lambda and competition terms
 join_comp_lambda$type <- factor(join_comp_lambda$type, levels = c("reference", "constructed"))
-ggplot(join_comp_lambda , aes(x = lambda, y = comp)) +
+ggplot(join_comp_lambda , aes(x = lambda, y = comp, col = type)) +
   geom_point()+
   theme_bw()+
   geom_text(aes(label = join_comp_lambda$Year), hjust = 0, vjust =0)+
   labs(y = "Competition term", x = "Lambda") +
-  xlim(0, 75)+
+  xlim(0, 80)+
   facet_wrap(~type)
+
+###############################
+#competition term with just EG#
+###############################
+Cx <- c()
+comp.EG.fxn <- function(a1, EG){
+  for(i in 1:17){
+    Cx[i] = 1-(1/(1+(a1[i]*EG[i])))
+  }  
+  return(Cx)
+}
+
+compEG <- comp.EG.fxn(a1 =  alpha_EG_mean[,5],
+                      EG = as.numeric(EG_mean[,2])) %>%
+  as.data.frame() # for constructed pools
+
+comp_refEG <- comp.EG.fxn(a1 =  refalpha_EG_mean[,5],
+                          EG = as.numeric(refEG_mean[,2])) %>%
+  as.data.frame()# for reference pools
+
+# Make a competition term table 
+comp_refEG <- as.data.frame(comp_refEG[-c(14,15,16,17),])%>%
+  mutate(Year = c(2003:2015))
+colnames(comp_refEG) <- c("reference", "Year")  
+compEG <- as.data.frame(compEG[-c(1,2,16,17),]) %>%
+  mutate(Year = c(2003:2015))
+colnames(compEG) <- c("constructed", "Year")
+comp_const_refEG <- merge(comp_refEG, compEG) %>%
+  gather(`reference`, `constructed`, key = type, value = comp)
+
+# Join lambda and competition tables
+join_comp_lambdaEG <- left_join(lambda_const_ref, comp_const_refEG, by = c("Year", "type"))
+
+join_comp_lambdaEG$type <- factor(join_comp_lambdaEG$type, levels = c("reference", "constructed"))
+ggplot(join_comp_lambdaEG , aes(x = lambda, y = comp)) +
+  geom_point()+
+  theme_bw()+
+  geom_text(aes(label = join_comp_lambdaEG$Year), hjust = 0, vjust =0)+
+  labs(y = "Competition term EG", x = "Lambda") +
+  xlim(0, 80)+
+  facet_wrap(~type)
+
+#################################
+#competition term with just LACO#
+#################################
+Cx <- c()
+comp.LACO.fxn <- function(aii, LACO){
+  for(i in 1:17){
+    Cx[i] = 1-(1/(1+(aii[i]*LACO[i])))
+  }  
+  return(Cx)
+}
+
+compLACO <- comp.LACO.fxn(aii =  alpha_LACO_mean[,5],
+                          LACO = as.numeric(LACO_mean[,2])) %>%
+  as.data.frame() # for constructed pools
+
+comp_refLACO <- comp.LACO.fxn(aii =  refalpha_LACO_mean[,5],
+                              LACO = as.numeric(refLACO_mean[,2])) %>%
+  as.data.frame()# for reference pools
+
+# Make a competition term table 
+comp_refLACO <- as.data.frame(comp_refLACO[-c(14,15,16,17),])%>%
+  mutate(Year = c(2003:2015))
+colnames(comp_refLACO) <- c("reference", "Year")  
+compLACO <- as.data.frame(compLACO[-c(1,2,16,17),]) %>%
+  mutate(Year = c(2003:2015))
+colnames(compLACO) <- c("constructed", "Year")
+comp_const_refLACO <- merge(comp_refLACO, compLACO) %>%
+  gather(`reference`, `constructed`, key = type, value = comp)
+
+# Join lambda and competition tables
+join_comp_lambdaLACO <- left_join(lambda_const_ref, comp_const_refLACO, by = c("Year", "type"))
+
+join_comp_lambdaLACO$type <- factor(join_comp_lambdaLACO$type, levels = c("reference", "constructed"))
+ggplot(join_comp_lambdaLACO , aes(x = lambda, y = comp)) +
+  geom_point()+
+  theme_bw()+
+  geom_text(aes(label = join_comp_lambdaEG$Year), hjust = 0, vjust =0)+
+  labs(y = "Competition term LACO", x = "Lambda") +
+  xlim(0, 80)+
+  facet_wrap(~type)
+
 
