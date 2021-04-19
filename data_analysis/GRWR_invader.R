1014d652984c9b92a837c1e9bc56090fe0e84b0c
Lina Aoyama <laoyama@uoregon.edu>
2021-04-19 10:07:09 -0700

    new file for growth rate when rare

---
+++ data_analysis/GRWR_invader.R
@@ -0,0 +1,12 @@
+#Goal: Calculate the long-term growth rate when rare (r_invader) of LACO 
+
+#Step 1. Calculate the stable equilibrium frequency of non-LACO species in the model each year. 
+#Step 2. Calculate the annual growth rate of LACO in restored pools when one LACO is introduced a stable community. 
+#Step 3. Do the same step for the reference pools.
+#Step 4. Average the growth rates of LACO over time for restored and reference pools.
+
+#--------------------------
+
+# Load data and package
+# Remember to set your data pathway first!
+source("data_compiling/compile_composition.R") 
