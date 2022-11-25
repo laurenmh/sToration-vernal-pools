31a6b3193ef785083f894833dc48213095b91e3d
Lauren Hallett <hallett@uoregon.edu>
2022-11-25 14:20:23 -0800

    streamlining fig 4 code

--- data_analysis/TREE_fig4.R
+++ data_analysis/TREE_fig4.R
@@ -1,162 +1,126 @@
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
-
-###DATA
-
-#csv files located in sToration-vernal-pools > data_analysis
-
-#data for LACO density (C)
-#"mean_join_LACO.csv"
-mean_join_LACO <- read_csv("mean_join_LACO.csv")
+library(cowplot)
 
 
-#data for LACO GRWR (D)
-#"Partitioning_GRWR_LACO_const.csv"
-#"Partitioning_GRWR_LACO_ref.csv"
-Partitioning_GRWR_LACO_const <- read_csv("Partitioning_GRWR_LACO_const.csv")
-Partitioning_GRWR_LACO_ref <- read_csv("Partitioning_GRWR_LACO_ref.csv")
+
+# Depth
+
+const_LACO_depth <- read_csv("const_LACO_depth.csv") %>%
+  mutate(Depth = recode(Depth, deep = "Deep", medium = "Medium", shallow = "Shallow")) %>%
+  mutate(`Pool depth` = factor(Depth, levels = c("Deep", "Medium", "Shallow" )))
 
 
-#data for LACO by depth (F)
-#"const_LACO_depth.csv" 
-const_LACO_depth <- read_csv("const_LACO_depth.csv")
+
+f <- ggplot(const_LACO_depth, aes(x = Year, y = mean_LACO, color = `Pool depth`)) + 
+  geom_point() + geom_line() +
+  labs(x = "Year", y =  bquote(`Lasthenia density`~(stems/m^2))) +
+  geom_errorbar(aes(ymin = mean_LACO - se_LACO, ymax = mean_LACO + se_LACO)) +
+  scale_y_log10() + 
+  theme_classic(base_size = 8) +
+  theme(legend.position = c(0.1, 0.2), legend.margin = margin(0, 0, 0, 0, "cm"),
+        legend.background = element_blank(), 
+        legend.box.background = element_blank(),
+        legend.key = element_blank()) +
+  scale_color_manual(values = c("#023858", "#3690c0", "#d0d1e6"))
+
+# Depth simulations
 
 
-#distribution of GRWR by depth for new (G) - rows = iterations, columns = years
-#"GRWR_LACO_const_deep.csv"
-#"GRWR_LACO_const_medium.csv"
-#"GRWR_LACO_const_shallow.csv"
+deep <- read_csv("GRWR_LACO_const_deep_mean.csv") %>%
+  mutate(Depth = "Deep")
+medium <- read_csv("GRWR_LACO_const_medium_mean.csv") %>%
+  mutate(Depth = "Medium")
+shallow <- read_csv("GRWR_LACO_const_shallow_mean.csv") %>%
+  mutate(Depth = "Shallow")
 
-GRWR_LACO_const_deep <- read_csv("GRWR_LACO_const_deep.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(meandeep = mean(c(V1:V15))) 
-
-GRWR_LACO_const_medium <- read_csv("GRWR_LACO_const_medium.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(meanmedium = mean(c(V1:V15)))
-
-GRWR_LACO_const_shallow <- read_csv("GRWR_LACO_const_shallow.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(meanshallow = mean(c(V1:V15)))
+depthtog <- rbind(deep, medium, shallow) %>%
+  mutate(`Pool depth` = factor(Depth, levels = c("Deep", "Medium", "Shallow" )))
 
 
-depthdist <- as_tibble(cbind(GRWR_LACO_const_deep$meandeep, GRWR_LACO_const_medium$meanmedium, GRWR_LACO_const_shallow$meanshallow))
-names(depthdist) = c("Deep", "Medium", "Shallow")
-
-depthdist2 <- pivot_longer(depthdist, 1:3, "Depth")
-
-g <- ggplot(depthdist2, aes(x=value, color = Depth, fill = Depth)) + 
+g <- ggplot(depthtog, aes(x=mean_GRWR, color = `Pool depth`, fill=`Pool depth`)) + 
   geom_density(alpha = .2, bw = 0.04) +
   theme_classic(base_size = 8) +
   theme(legend.position = c(0.2, 0.8), legend.margin = margin(0, 0, 0, 0, "cm"),
-        legend.background = element_blank(), legend.box.background = element_blank(),
+        legend.background = element_blank(), 
+        legend.box.background = element_blank(),
         legend.key = element_blank()) + 
-  labs(x = "Low-density growth rate", y = "Probability density") +
-  geom_vline(xintercept = 0, alpha = 0.5, linetype = "dashed") 
-
-
-#data for avg EG timeseries (I)
-#"EG_all.csv"
-EG_all <- read_csv("EG_all.csv")
-
-# #distribution of GRWR by EG reduction for new (J) - rows = iterations, columns = years
-# #"GRWR_LACO_const.csv" - 0% EG reduction
-# GRWR_LACO_const <- read_csv("GRWR_LACO_const.csv") %>%
-#   select(-1) %>%
-#   rowwise() %>%
-#   mutate(meanconst = mean(c(V1:V15)))
-# 
-# GRWR_LACO_const2 <- read_csv("GRWR_LACO_const.csv") %>%
-#   select(-1) %>%
-#   pivot_longer(1:15, "year")
-#   
-# mean(GRWR_LACO_const2$value)
-# ggplot(GRWR_LACO_const2, aes(x=value)) + geom_density()
-
-#"GRWR_LACO_50EG.csv" - 50% EG reduction
-GRWR_LACO_50EG <- read_csv("GRWR_LACO_50EG.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(mean50 = mean(c(V1:V15)))
-
-GRWR_LACO_50EG_mean <- read_csv("GRWR_LACO_50EG.csv") %>%
-  magrittr::set_colnames(c(2001:2015)) %>%
-  rownames_to_column(., var = "iteration") %>% #2000 iterations from Bayesian modeling
-  pivot_longer(!iteration, names_to = "Year", values_to = "GRWR") %>%
-  group_by(iteration) %>%
-  summarize(mean_GRWR = mean(GRWR))  #mean of GRWR across years
-
-#"GRWR_LACO_75EG.csv" - 75% EG reduction
-GRWR_LACO_75EG <- read_csv("GRWR_LACO_75EG.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(mean75 = mean(c(V1:V15)))
-
-grasssim <- as_tibble(cbind(GRWR_LACO_const$meanconst, GRWR_LACO_50EG$mean50, GRWR_LACO_75EG$mean75))
-names(grasssim) = c("No reduction", "50% reduction", "75% reduction")
-
-grasssim2 <- pivot_longer(grasssim, 1:3, "Scenario")
-
-ggplot(grasssim2, aes(x=value, color = Scenario, fill = Scenario)) +
-  geom_density(alpha = .2, bw = 0.04) +
-  theme_classic(base_size = 8) +
-  theme(legend.position = c(0.2, 0.8), legend.margin = margin(0, 0, 0, 0, "cm"),
-        legend.background = element_blank(), legend.box.background = element_blank(),
-        legend.key = element_blank()) + 
-  labs(x = "Low-density growth rate", y = "Probability density") +
-  geom_vline(xintercept = 0, alpha = 0.5, linetype = "dashed") 
-
+  xlim(-2, 1.2) +
+  labs(x = "Lasthenia low-density growth rate", y = "Probability density") +
+  geom_vline(xintercept = 0, alpha = 0.5, linetype = "dashed") +
+  geom_point(data = . %>%
+               group_by(`Pool depth`) %>%
+               summarize(mean = mean(mean_GRWR)) %>%
+               mutate(y = -0.2),
+             aes(x = mean, y = y, colour = `Pool depth`), size = 2) +
+  scale_color_manual(values = c("#023858", "#3690c0", "#d0d1e6")) +
+  scale_fill_manual(values = c("#023858", "#3690c0", "#d0d1e6"))
   
 
-## alternative 
-
-GRWR_LACO_const2 <- read_csv("GRWR_LACO_const.csv") %>%
-  select(-1) %>%
-  summarize_all(.funs="mean") %>%
-  pivot_longer(1:15, "year")
-
-ggplot(GRWR_LACO_const2, aes(x=value)) +
-  geom_density()
+# invasive grass patterns 
+const_EG_depth <- read_csv("const_EG_depth.csv") %>%
+  mutate(Depth = recode(Depth, deep = "Deep", medium = "Medium", shallow = "Shallow")) %>%
+  mutate(`Pool depth` = factor(Depth, levels = c("Deep", "Medium", "Shallow" )))
 
 
-meanconst <- GRWR_LACO_const$meanconst
 
-ggplot(GRWR_LACO_const, aes(x=meanconst)) + geom_density()
+i <- ggplot(const_EG_depth, aes(x = Year, y = mean_EG, color = `Pool depth`)) + 
+  geom_point() + geom_line() +
+  labs(x = "Year", y =  "Annual grass density (% cover)") +
+  geom_errorbar(aes(ymin = mean_EG - se_EG, ymax = mean_EG + se_EG)) +
+  theme_classic(base_size = 8) +
+  theme(legend.position = c(0.7, 0.15), legend.margin = margin(0, 0, 0, 0, "cm"),
+        legend.background = element_blank(), legend.box.background = element_blank(),
+        legend.key = element_blank()) +
+  scale_color_manual(values = c("#023858", "#3690c0", "#d0d1e6")) 
+  
 
-#"GRWR_LACO_50EG.csv" - 50% EG reduction
-GRWR_LACO_50EG <- read_csv("GRWR_LACO_50EG.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(mean50 = mean(c(V1:V15)))
 
-#"GRWR_LACO_75EG.csv" - 75% EG reduction
-GRWR_LACO_75EG <- read_csv("GRWR_LACO_75EG.csv") %>%
-  select(-1) %>%
-  rowwise() %>%
-  mutate(mean75 = mean(c(V1:V15)))
+## INVasive grass simulations
 
-grasssim <- as_tibble(cbind(GRWR_LACO_const$meanconst, GRWR_LACO_50EG$mean50, GRWR_LACO_75EG$mean75))
-names(grasssim) = c("No reduction", "50% reduction", "75% reduction")
+GRWR_LACO_50EG_mean <- read_csv("GRWR_LACO_50EG_mean.csv") %>%
+  mutate(Scenario = "50% grass removal")
+GRWR_LACO_75EG_mean <- read_csv("GRWR_LACO_75EG_mean.csv") %>%
+  mutate(Scenario = "75% grass removal")
+GRWR_LACO_const_mean <- read_csv("GRWR_LACO_const_mean.csv") %>%
+  mutate(Scenario = "No grass removal")
 
-grasssim2 <- pivot_longer(grasssim, 1:3, "Scenario")
+simtog <- rbind(GRWR_LACO_50EG_mean, GRWR_LACO_75EG_mean, GRWR_LACO_const_mean) %>%
+  mutate(Scenario = factor(Scenario, levels = c("No grass removal", "50% grass removal", "75% grass removal" )))
 
-ggplot(grasssim2, aes(x=value, color = Scenario)) +
-  geom_density()
 
-#extras
-#"GRWR_depth_all.csv" - old (G) summarized GRWR by depth
-#"const_EG_depth.csv" - old (I) EG by depth
-#"GRWR_simulated_all.csv"- old (J) summarized GRWR by EG reduction
+j <- ggplot(simtog, aes(x=mean_GRWR, color = Scenario, fill=Scenario)) + 
+  geom_density(alpha = .2, bw = 0.04) +
+  theme_classic(base_size = 8) +
+  theme(legend.position = c(0.35, 0.8), legend.margin = margin(0, 0, 0, 0, "cm"),
+        legend.background = element_blank(), 
+        legend.box.background = element_blank(),
+        legend.key = element_blank()) + 
+  xlim(-.6, .6) +
+  labs(x = "Lasthenia low-density growth rate", y = "Probability density") +
+  geom_vline(xintercept = 0, alpha = 0.5, linetype = "dashed") + 
+  geom_point(data = . %>%
+               group_by(Scenario) %>%
+               summarize(mean = mean(mean_GRWR)) %>%
+               mutate(y = -0.2),
+             aes(x = mean, y = y, colour = Scenario), size = 2) +
+  scale_color_manual(values = c("#662506", "#ec7014", "#fee391")) +
+  scale_fill_manual(values = c("#662506", "#ec7014", "#fee391"))
 
-###PLOTS
+plot_grid(f,g,i,j)
 
+ggsave(filename = "fig4a.pdf", width = 9, height = 6)
+
+# PUT THEM TOGETHER
+
+
+mean_join_LACO <- read_csv("mean_join_LACO.csv") %>%
+  mutate(type = recode(type, constructed = "Constructed", reference = "Reference"),
+         `Pool type` = factor(type, levels = c("Reference", "Constructed")))
 #(C) LACO density
 c <- ggplot(mean_join_LACO%>%filter(Year %in% c(2000:2015)), 
-       aes(x = Year, y = mean_LACOdens, col = type)) +
+       aes(x = Year, y = mean_LACOdens, col = `Pool type`)) +
   geom_point()+
   geom_line()+
   scale_y_log10()+
@@ -169,10 +133,9 @@ c <- ggplot(mean_join_LACO%>%filter(Year %in% c(2000:2015)),
   #       axis.line = element_line(colour = "black"),
   #       legend.position = c(.2, .3),
   #       axis.title = element_text(size = 14))+
-  ylab(bquote(Density~(stems/m^2))) +
-  xlab("Year") +
-  scale_x_continuous(name = NULL, limits = c(1999.5,2015.5))+
-  scale_color_manual(name = "", values = c("#000000", "#888888")) +
+  labs(y = bquote(`Lasthenia density`~(stems/m^2)), x = "Year") +
+  scale_x_continuous(limits = c(1999.5,2015.5))+
+  scale_color_manual( values = c("#000000", "#888888")) +
   theme_classic(base_size = 8) +
   theme(legend.position = c(0.3, 0.3), legend.margin = margin(0, 0, 0, 0, "cm"),
         legend.background = element_blank(), legend.box.background = element_blank(),
@@ -194,7 +157,7 @@ Part_const <- ggplot(Partitioning_GRWR_LACO_const, aes(x = mechanism, y = Mean, 
   geom_hline(yintercept = 0)+
   scale_fill_manual(values = c("grey27", "grey60", "grey60", "grey60", "grey60"))+
   ylim(-1.2, 1.2)+
-  annotate("text", x = 2, y = 1.1, label = "Constructed", size = 6)+
+  annotate("text", x = 2, y = 1.1, label = "Constructed", size = 2)+
   scale_x_discrete(labels = xlabels)
 
 Partitioning_GRWR_LACO_ref$mechanism <- ordered(Partitioning_GRWR_LACO_ref$mechanism, levels = c("r_overall", "epsilon_0", "epsilon_alpha", "epsilon_lambda", "epsilon_int"))
@@ -206,24 +169,36 @@ Part_ref <- ggplot(Partitioning_GRWR_LACO_ref, aes(x = mechanism, y = Mean, fill
   geom_hline(yintercept = 0)+
   scale_fill_manual(values = c("grey27", "grey60", "grey60", "grey60", "grey60"))+
   ylim(-1.2, 1.2)+
-  annotate("text", x = 2, y = 1.1, label = "Reference", size = 6)+
+  annotate("text", x = 2, y = 1.1, label = "Reference", size = 2)+
   scale_x_discrete(labels = xlabels)
-figure_partitioning <- ggarrange(Part_ref, Part_const, ncol = 2, nrow = 1, legend = "none", 
-                                 labels = c("(a)", "(b)"), font.label = list(size = 20))
-annotate_figure(figure_partitioning, bottom = text_grob("Mechanisms", size = 20),
-                left = text_grob("Partitioning of Low Density Growth Rate", size = 18, rot = 90))
-d <- figure_partitioning
 
-#(F) LACO by depth
-f <- ggplot(const_LACO_depth,aes(x = Year, y = mean_LACO, color = Depth)) + 
-  geom_point() + geom_line() +
-  labs(x = "Year", y =  bquote(Density~(stems/m^2))) +
-  geom_errorbar(aes(ymin = mean_LACO - se_LACO, ymax = mean_LACO + se_LACO)) +
-  scale_y_log10() + 
-  theme_classic(base_size = 8) +
-  theme(legend.position = c(0.8, 0.8), legend.margin = margin(0, 0, 0, 0, "cm"),
-        legend.background = element_blank(), legend.box.background = element_blank(),
-        legend.key = element_blank()) 
+figure_partitioning <- ggarrange(Part_ref, Part_const, ncol = 2, nrow = 1, legend = "none")
+
+d <- annotate_figure(figure_partitioning, bottom = text_grob("Mechanisms", size = 8),
+                left = text_grob("Partitioning of low-density growth rate", size = 8, rot = 90))
+ggsave(filename = "fig4a2.pdf", width = 3, height = 3)
+
+plot_grid(c,d)
+
+ggsave(filename = "fig4b.pdf", width = 9, height = 3)
+
+c
+ggsave(filename = "fig4a1.pdf", width = 3, height = 3)
+
+
+
+
+# 
+# #(F) LACO by depth
+# f <- ggplot(const_LACO_depth,aes(x = Year, y = mean_LACO, color = Depth)) + 
+#   geom_point() + geom_line() +
+#   labs(x = "Year", y =  bquote(Density~(stems/m^2))) +
+#   geom_errorbar(aes(ymin = mean_LACO - se_LACO, ymax = mean_LACO + se_LACO)) +
+#   scale_y_log10() + 
+#   theme_classic(base_size = 8) +
+#   theme(legend.position = c(0.8, 0.8), legend.margin = margin(0, 0, 0, 0, "cm"),
+#         legend.background = element_blank(), legend.box.background = element_blank(),
+#         legend.key = element_blank()) 
 
 # ggplot(const_LACO_depth, aes(x = Year, y = mean_LACO, col = Depth))+
 #   geom_point()+
